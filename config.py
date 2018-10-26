42856ba9b6f1c2bab2489cd5b432cbb8c45ed49b
Sam Wood <41810698+swood15@users.noreply.github.com>
2018-10-25 22:58:26 -0700

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Add your API key
-owm_key = "296377f1e549da791b8add68d55cade0"
-nyt_key = "a8c29774af88442db2d5c5343b297f39"
-gmap_key = "AIzaSyCxHHJZCNFwo1FQupLY6mknIM3fKAv14PU"
-cens_key = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
